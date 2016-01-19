0e35924cd3327be339fdd4a1dacb6814ed0d6ce3
mengyongming <mengyongming@acfun.tv>
2016-01-19 16:17:25 +0800

    goku 基础架构-jetty容器启动

---
+++ gk-fw-boot/gk-fw-boot-jetty/src/main/java/com/gkframework/boot/Main.java
@@ -0,0 +1,130 @@
+package com.gkframework.boot;
+
+import com.gkframework.boot.core.Container;
+import com.gkframework.boot.core.log4j.Log4jContainer;
+import com.gkframework.boot.core.log4j.LoggingConfig;
+import com.gkframework.boot.core.monitor.MonitorConfig;
+import com.gkframework.boot.core.monitor.MonitorContainer;
+import com.gkframework.boot.jetty.spring.SpringContainer;
+import com.gkframework.boot.jetty.spring.SpringContainerManager;
+import com.gkframework.config.core.EnvConfigurer;
+import org.apache.log4j.Logger;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * spring+Jetty的容器
+ *
+ * @author moueimei
+ */
+public class Main {
+
+	public static final String SHUTDOWN_HOOK_KEY = "shutdown.hook";
+
+	private static Logger logger = Logger.getLogger(Main.class);
+
+	protected static volatile boolean running = true;
+
+	public static void main(String[] args) {
+		try {
+			for (int i = 0; i < args.length; i++) {
+				String envArgs = args[i];
+				if (envArgs.indexOf("env=") > -1) {
+					String[] envs = envArgs.split("=");
+					EnvConfigurer.env = envs[1];
+					logger.warn("配置项：env=" + EnvConfigurer.env);
+				}
+			}
+
+			MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+
+			final List<Container> containers = new ArrayList<Container>();
+			Container springContainer = new SpringContainer();
+			Container log4jContainer = new Log4jContainer();
+			Container monitorContainer = new MonitorContainer();
+			containers.add(log4jContainer);
+			containers.add(monitorContainer);
+			containers.add(springContainer);
+
+			if ("true".equals(System.getProperty(SHUTDOWN_HOOK_KEY))) {
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					public void run() {
+						for (Container container : containers) {
+							try {
+								container.stop();
+								logger.info("Server " + container.getName() + " stopped!");
+							} catch (Throwable t) {
+								logger.error(t.getMessage(), t);
+							}
+							synchronized (Main.class) {
+								running = false;
+								Main.class.notify();
+							}
+						}
+					}
+				});
+			}
+
+			for (Container container : containers) {
+				try {
+
+					Hashtable<String, String> properties = new Hashtable<String, String>();
+
+					properties.put(Container.TYPE_KEY, Container.DEFAULT_TYPE);
+					properties.put(Container.ID_KEY, container.getType());
+
+					ObjectName oname = ObjectName.getInstance("tv.acframework", properties);
+					if(container instanceof SpringContainer) {
+						SpringContainerManager mbean = new SpringContainerManager();
+
+						if (mbs.isRegistered(oname)) {
+							mbs.unregisterMBean(oname);
+						}
+						mbs.registerMBean(mbean, oname);
+					}
+					else if(container instanceof Log4jContainer) {
+						LoggingConfig mbean = new LoggingConfig();
+
+						if (mbs.isRegistered(oname)) {
+							mbs.unregisterMBean(oname);
+						}
+						mbs.registerMBean(mbean, oname);
+					}
+					else if(container instanceof MonitorContainer) {
+						MonitorConfig mbean = new MonitorConfig();
+
+						if (mbs.isRegistered(oname)) {
+							mbs.unregisterMBean(oname);
+						}
+						mbs.registerMBean(mbean, oname);
+					}
+				} catch (Exception e) {
+					logger.error("注册JMX服务出错：" + e.getMessage(), e);
+				}
+				logger.warn("服务 " + container.getType() + " 启动中!");
+				container.start();
+
+			}
+			logger.warn(new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]").format(new Date()) + " 所有服务启动成功!");
+		} catch (RuntimeException e) {
+			logger.error(e.getMessage(), e);
+			System.exit(1);
+		}
+		synchronized (Main.class) {
+			while (running) {
+				try {
+					Main.class.wait();
+				} catch (Throwable e) {
+				}
+			}
+		}
+	}
+
+}
---
+++ gk-fw-boot/gk-fw-boot-jetty/src/main/java/com/gkframework/boot/jetty/spring/SpringContainerManager.java
@@ -0,0 +1,41 @@
+package com.gkframework.boot.jetty.spring;
+
+
+import com.gkframework.boot.core.Container;
+
+/**
+ * Main. (API, Static, ThreadSafe)
+ * 
+ * @author Cruise.Xu
+ */
+public class SpringContainerManager implements SpringContainerManagerMBean {
+
+	Container springContainer = new SpringContainer();
+
+	@Override
+	public String getName() {
+		return springContainer.getName();
+	}
+
+	@Override
+	public void restart() {
+		springContainer.restart();
+	}
+
+	@Override
+	public void start() {
+		springContainer.start();
+	}
+
+	@Override
+	public void stop() {
+		springContainer.stop();
+
+	}
+
+	@Override
+	public boolean isRunning() {
+		return springContainer.isRunning();
+	}
+
+}
