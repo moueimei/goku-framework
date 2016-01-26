50c900e59192e4cabf27a14c9f782d3f8e95b4ce
mengyongming <mengyongming@acfun.tv>
2016-01-26 11:22:19 +0800

    goku 基础架构-dubbo服务质量模型

---
+++ gk-fw-qos/gk-fw-qos-server/gk-fw-qos-server-core/src/main/java/com/gkframework/qos/server/core/entity/AppInfo.java
@@ -0,0 +1,63 @@
+package com.gkframework.qos.server.core.entity;
+
+public class AppInfo extends BaseEntity {
+
+	private static final long serialVersionUID = -1473872193332974750L;
+
+	private String name;
+	private String code;
+
+	private Short state;
+	private Short isAuto;
+	private Integer disorder;
+	private String remark;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Short getState() {
+		return state;
+	}
+
+	public void setState(Short state) {
+		this.state = state;
+	}
+
+	public Integer getDisorder() {
+		return disorder;
+	}
+
+	public void setDisorder(Integer disorder) {
+		this.disorder = disorder;
+	}
+
+	public Short getIsAuto() {
+		return isAuto;
+	}
+
+	public void setIsAuto(Short isAuto) {
+		this.isAuto = isAuto;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+}
